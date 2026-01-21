a0affefc4ecf9f574b6e11bd38a06196b249d2df
Alex130220 <dixlofos80@gmail.com>
2026-01-21 21:13:17 +0300

    Initial commit
    Добавлены Django бэкенд и React фронтенд

--- backend/pigeon/settings.py
+++ backend/pigeon/settings.py
@@ -123,6 +123,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'templates',
+            BASE_DIR / 'pigeon/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
