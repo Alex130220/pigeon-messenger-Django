59a18791f63f6e1f14ff46005800887747c353a5
Alex130220 <dixlofos80@gmail.com>
2026-01-22 14:40:20 +0300

    Initial commit
    Добавлены Django бэкенд и React фронтенд

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+ def ready(self):
+        # Импортируем сигналы если есть
+        import users.signals
