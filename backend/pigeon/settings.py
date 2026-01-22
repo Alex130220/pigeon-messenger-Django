6106cf43d82ea38abfd0904f34360d8b15732f6d
Alex130220 <dixlofos80@gmail.com>
2026-01-22 14:45:51 +0300

    Initial commit
    Добавлены Django бэкенд и React фронтенд

--- backend/pigeon/settings.py
+++ backend/pigeon/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'users',
     'messenger.apps.MessengerConfig',
     'channels',
     'notifications',
